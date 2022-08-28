c4cb55d413afc488ca85df57fdc550ee2239a4ba
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2022-08-28 13:24:10 -0500

    added test rotation
    Only click around the screen to rotate at 90 degrees. Just a test.

---
+++ Lost Lights/Assets/Scripts/Rotation.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float speed = 1.5f;
+    private Vector3 target;
+    private Vector3 target_rot;
+    private bool planetClick;
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    void OnMouseDown()
+    {
+        planetClick = true;
+    }
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            if (planetClick) 
+            {
+                target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                target.z = transform.position.z;
+                target_rot = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            }
+
+        }
+     
+        var angle = Mathf.Atan2(target_rot.y - transform.position.y, target_rot.x - transform.position.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle + 90);
+    }
+
+
+}
