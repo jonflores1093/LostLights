98612acb014bf53c274e0809d9ad505404ee13aa
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2022-10-01 22:56:55 -0500

    added moving background to menu scene
    added background that moves in the menu screen but the seams make it look bad.

--- Lost Lights/Assets/Lost Lights/Scripts_LL/Questions.cs
+++ Lost Lights/Assets/Lost Lights/Scripts_LL/Questions.cs
@@ -2,33 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Questions : MonoBehaviour
+namespace InsertStudioLostLights
 {
-    public GameObject[] moonTypes;
-    public int randomQuestion;
-
-
-    //static public List<Questions> moonList;
-
-    private void Awake()
+    public class Questions : MonoBehaviour
     {
-        RandomQuestion();
+        public GameObject[] moonTypes;
+        public int randomQuestion;
+
+
+        //static public List<Questions> moonList;
+
+        private void Awake()
+        {
+            RandomQuestion();
+        }
+
+        //private void Start()
+        //{
+        //    moonList = new List<Questions>();
+        //}
+
+
+        public void RandomQuestion()
+        {
+            randomQuestion = (int)Random.Range(0, moonTypes.Length);
+            Instantiate(moonTypes[randomQuestion], transform.position, transform.rotation);
+        }
+
+
+
+
+
     }
-
-    //private void Start()
-    //{
-    //    moonList = new List<Questions>();
-    //}
-
-
-    public void RandomQuestion()
-    {
-        randomQuestion = (int)Random.Range(0, moonTypes.Length);      
-        Instantiate(moonTypes[randomQuestion], transform.position, transform.rotation);
-    }
-
-    
-
-
-    
 }
