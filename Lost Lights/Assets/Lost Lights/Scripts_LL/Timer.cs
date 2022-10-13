35567dee8ab0d862f2deaa3c830c7f38d8a7620a
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2022-10-12 19:06:06 -0500

    Added script, ui and text.

---
+++ Lost Lights/Assets/Lost Lights/Scripts_LL/Timer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Timer : MonoBehaviour
+{
+    public class Clock
+    {
+        private System.DateTime now;
+        private System.TimeSpan timeNow;
+        private System.TimeSpan gameTime;
+        private int minutesPerDay; //Realtime minutes per game-day (1440 would be realtime)
+        public TMP_Text TimeDay;
+
+        public Clock(int minPerDay)
+        {
+            minutesPerDay = minPerDay;
+        }
+
+        public System.TimeSpan GetTime()
+        {
+            now = System.DateTime.Now;
+            timeNow = now.TimeOfDay;
+            double hours = timeNow.TotalMinutes % minutesPerDay;
+            double minutes = (hours % 1) * 60;
+            double seconds = (minutes % 1) * 60;
+            gameTime = new System.TimeSpan((int)hours, (int)minutes, (int)seconds);
+
+            return gameTime;
+        }
+    }
+
+    Clock clock;
+
+    void Start()
+    {
+        clock = new Clock(24);
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(clock.GetTime().ToString());
+    }
+}
+
+
+
+
