567a672499cb00dcd4e94da753d1aa2de5085323
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2022-11-06 15:11:02 -0600

    Added prefabs and new scene

--- Lost Lights/Assets/Lost Lights/Scripts_LL/RotationRight.cs
+++ Lost Lights/Assets/Lost Lights/Scripts_LL/RotationRight.cs
@@ -13,7 +13,7 @@ namespace InsertStudioLostLights
     public class RotationRight : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
 
-        public GameObject rotatedObject, rotationlightButton;
+        public GameObject rotatedObjectMain, rotationlightButton, rotatedObjectMini,orbit;
         public RawImage bgMovement;
         public float xbg;
         public Timer clock;
@@ -29,7 +29,10 @@ namespace InsertStudioLostLights
         {
             if (rotate == false)
                 return;
-            rotatedObject.transform.Rotate(Vector3.back * rot_Speed * Time.deltaTime);
+            rotatedObjectMain.transform.Rotate(Vector3.back * rot_Speed * Time.deltaTime);
+            rotatedObjectMini.transform.Rotate(Vector3.back * rot_Speed * Time.deltaTime);
+            orbit.transform.Rotate(Vector3.back * rot_Speed * Time.deltaTime);
+
             rotationlightButton.transform.Rotate(Vector3.up * rot_Speed * Time.deltaTime);
             bgMovement.uvRect = new Rect(bgMovement.uvRect.position + new Vector2(xbg,0) * Time.deltaTime, bgMovement.uvRect.size);
 
