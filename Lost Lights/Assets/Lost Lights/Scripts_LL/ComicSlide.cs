13847425cc86216dd40996ae61b30eff6a7ebdd0
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2023-01-03 18:06:53 -0600

    position testing

--- Lost Lights/Assets/Lost Lights/Scripts_LL/ComicSlide.cs
+++ Lost Lights/Assets/Lost Lights/Scripts_LL/ComicSlide.cs
@@ -11,17 +11,30 @@ namespace InsertStudioLostLights
 {
     public class ComicSlide : MonoBehaviour
     {
+        public Camera MainCamera;
+        //public GameObject scrollLeft;
+        private Vector3 next1 = new Vector3(9.7f, 0, 0);
+        private Vector2 next2 = new Vector2(23.9f, 0);
+        private Vector2 next3 = new Vector2(39.2f, 0);
+        private Vector2 next4 = new Vector2(56.5f, 0);
+        private Vector2 next5 = new Vector2(75.3f, 0);
+        private Vector2 next6 = new Vector2(92.1f, 0);
+        private Vector2 next7 = new Vector2(92.1f, 0);
+        private Vector2 next8 = new Vector2(105.6f, 0);
+        private Vector2 next9 = new Vector2(121.1f, 0);
+        private Vector2 next10 = new Vector2(138.9f, 0);
 
-        public GameObject scrollLeft;
 
-     
-       
+
 
         public void ClickNext()
         {
-       
 
-            scrollLeft.transform.Translate(12.8f, 0.38f, 0);
+
+            MainCamera.transform.position = new Vector3(9.7f, 0,0);
+
+            
+          
 
 
         }
