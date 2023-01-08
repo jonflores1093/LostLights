53e909a8825caadcdf1c39d7a5847592b37b3ff1
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2023-01-07 18:52:42 -0600

    Changes to questions and comic slide

--- Lost Lights/Assets/Lost Lights/Scripts_LL/ComicSlide.cs
+++ Lost Lights/Assets/Lost Lights/Scripts_LL/ComicSlide.cs
@@ -11,30 +11,29 @@ namespace InsertStudioLostLights
 {
     public class ComicSlide : MonoBehaviour
     {
+        public Button clicker;
         public Camera MainCamera;
+        private int clicks = 0;
         //public GameObject scrollLeft;
-        private Vector3 next1 = new Vector3(9.7f, 0, 0);
-        private Vector2 next2 = new Vector2(23.9f, 0);
-        private Vector2 next3 = new Vector2(39.2f, 0);
-        private Vector2 next4 = new Vector2(56.5f, 0);
-        private Vector2 next5 = new Vector2(75.3f, 0);
-        private Vector2 next6 = new Vector2(92.1f, 0);
-        private Vector2 next7 = new Vector2(92.1f, 0);
-        private Vector2 next8 = new Vector2(105.6f, 0);
-        private Vector2 next9 = new Vector2(121.1f, 0);
-        private Vector2 next10 = new Vector2(138.9f, 0);
+        private Vector3 next1 = new Vector3(9.7f, 0, -10);
+        private Vector2 next2 = new Vector3(23.9f, 0, -10);
+        private Vector2 next3 = new Vector3(39.2f, 0, -10);
+        private Vector2 next4 = new Vector3(56.5f, 0, -10);
+        private Vector2 next5 = new Vector3(75.3f, 0, -10);
+        private Vector2 next6 = new Vector3(92.1f, 0, -10);
+        private Vector2 next7 = new Vector3(92.1f, 0, -10);
+        private Vector2 next8 = new Vector3(105.6f, 0, -10);
+        private Vector2 next9 = new Vector3(121.1f, 0, -10);
+        private Vector2 next10 = new Vector3(138.9f, 0, -10);
 
+     
 
-
-
+        
         public void ClickNext()
         {
 
-
-            MainCamera.transform.position = new Vector3(9.7f, 0,0);
-
-            
-          
+            MainCamera.transform.position = new Vector3(9.7f,0,-10);
+           
 
 
         }
