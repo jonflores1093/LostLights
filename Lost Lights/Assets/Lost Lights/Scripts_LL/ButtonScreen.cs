d65c1ff3d552310c58ce3ccb8752df01495513c8
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2023-02-15 20:36:52 -0600

    added buttons to explain certain screens

---
+++ Lost Lights/Assets/Lost Lights/Scripts_LL/ButtonScreen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using LoLSDK;
+using SimpleJSON;
+
+
+namespace InsertStudioLostLights
+{
+    public class ButtonScreen : MonoBehaviour
+    {
+
+    
+    }
+}
